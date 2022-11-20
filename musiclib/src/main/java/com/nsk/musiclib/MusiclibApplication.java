55a6e3f8e6d33dd346252236572fcb00db8f2177
nirajskhot <nirajskhot@gmail.com>
2022-11-20 16:19:50 +0530

    Initial Commit

---
+++ musiclib/src/main/java/com/nsk/musiclib/MusiclibApplication.java
@@ -0,0 +1,13 @@
+package com.nsk.musiclib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclibApplication.class, args);
+	}
+
+}
---
+++ musiclib/src/test/java/com/nsk/musiclib/MusiclibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsk.musiclib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiclibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
